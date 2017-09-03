70e8de5c41d429b6caa2c716d2d43a6cd08d1274
SirLameGame <SirLameGame@gmail.com>
2017-09-03 16:20:22 +0100

    start adding some of the suggested teams

---
+++ src/components/Home/Teams/data.js
@@ -0,0 +1,6 @@
+export teams = [
+  {name: 'Žalgiris', region: 'Kaunas', abbrev: 'BCZ', pop: .3, imgURL: 'http://i.imgur.com/jFe0U12.png'},
+  {name: 'Squires', region: 'Virginia', abbrev: 'SQR', pop: .2, imgURL: 'http://i.imgur.com/lH7jrt0.png'},
+  {name: 'Fake', region: 'Madrid', abbrev: 'FMD', pop: 3.1, imgURL: 'http://i.imgur.com/gyDf765.png'},
+  {name: 'Orcas', region: 'Juneau', abbrev: 'JUO', pop: .1, imgURL: 'http://i.imgur.com/zLpZedx.png'},
+]
